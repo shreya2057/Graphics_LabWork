941f9cedc993fa1d96aab9fb0c5ccf9f6ecb6112
Shreya Shrestha <shreya2057@gmail.com>
2021-08-02 10:19:59 +0545

    DDA and Bresenham Algorithm

---
+++ line_drawing/m_less/sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('white');
+  mless(30,50,100,115);
+}
+
+function mless(x1, y1, x2, y2){
+  x = x1;
+  y = y1;
+  strokeWeight(6);
+  point(x, y);
+  point(x2,y2);
+  dx = x2-x1;
+  dy = y2-y1;
+  pk = 2*dy-dx;
+  for(i = 0; i< dx; i++){
+    if(pk<0){
+      x = x + 1;
+      y = y;
+      pk = pk + 2*dy;
+      if(x == x2){
+        strokeWeight(6);
+        point(x,y);
+      }
+      else{
+        strokeWeight(3);
+        point(x,y);
+      }
+    }
+    else{
+      x = x + 1;
+      y = y + 1;
+      pk = pk + 2*dy -2*dx;
+      if(x == x2){
+        strokeWeight(6);
+        point(x,y);
+      }
+      else{
+        strokeWeight(3);
+        point(x,y);
+      }
+    }
+  }
+}
