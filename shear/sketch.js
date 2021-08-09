30d5fa135a54014ede388e1917233db27bf21d06
Shreya Shrestha <shreya2057@gmail.com>
2021-08-09 17:51:05 +0545

    Shear

--- shear/sketch.js
+++ shear/sketch.js
@@ -4,20 +4,12 @@ function setup() {
 
 function draw() {
   background(220);
-  x_shear(200,100,70,70);
+  x_shear(100,90,70,70,70,150,2);
+  y_shear(100,90,70,70,70,150,2);
 }
 
-  sx = 2;
-  sy = 2;
-  s = [
-    [sx, 0, 0],
-    [0 ,sy, 0],
-    [0 , 0, 1],
-  ];
-
-function x_shear(x1, y1,x2,y2, factor){
-  x = 10;
-  y = 20;
+function x_shear(x1, y1,x2,y2,x3,y3, factor){
+  shx = factor;
   strokeWeight(2);
   stroke('black');
   s = [
@@ -25,11 +17,54 @@ function x_shear(x1, y1,x2,y2, factor){
     [0 ,1, 0],
     [0 , 0, 1],
   ];
+  beginShape(line);
+  vertex(x1,y1);
+  vertex(x2,y2);
+  vertex(x3,y3);
+  vertex(x1,y1);
+  endShape();
   line(x1,y1,x2,y2);
   a = multiply(x1,y1,s);
-  // b = multiply(x2,y2);
+  b = multiply(x2,y2,s);
+  c = multiply(x3,y3,s);
   stroke('red');
-  line(a[0],a[1],b[0],b[1]);
+  beginShape(line);
+  vertex(a[0],a[1]);
+  vertex(b[0],b[1]);
+  vertex(c[0],c[1]);
+  vertex(a[0],a[1]);
+  endShape();
+  p = a[0] + 10;
+  strokeWeight(0);
+  text("x-axis shear", 350 ,350);
+  strokeWeight(6);
+  point(330,345);
+}
+
+function y_shear(x1, y1,x2,y2,x3,y3, factor){
+  shy = factor;
+  strokeWeight(2);
+  stroke('black');
+  s = [
+    [1, 0, 0],
+    [shy ,1, 0],
+    [0 , 0, 1],
+  ];
+  line(x1,y1,x2,y2);
+  a = multiply(x1,y1,s);
+  b = multiply(x2,y2,s);
+  c = multiply(x3,y3,s);
+  stroke('blue');
+  beginShape(line);
+  vertex(a[0],a[1]);
+  vertex(b[0],b[1]);
+  vertex(c[0],c[1]);
+  vertex(a[0],a[1]);
+  endShape();
+  strokeWeight(0);
+  text("y-axis shear", 350 ,370);
+  strokeWeight(6);
+  point(330,370);
 }
 
 function multiply(x,y,s){
