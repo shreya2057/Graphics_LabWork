1d1ec489f94a0bc73c8a5c8e6a53a052c7430971
Shreya Shrestha <shreya2057@gmail.com>
2021-08-09 20:51:27 +0545

    Reflection

--- reflection/sketch.js
+++ reflection/sketch.js
@@ -4,11 +4,11 @@ function setup() {
 
 function draw() {
   background(220);
-  x_reflect(100,90,70,70,70,150,2);
-  // y_shear(100,90,70,70,70,150,2);
+  // x_reflect(200,100,70,170);
+  y_reflect(300,100,200,170);
 }
 
-function x_reflect(x1, y1,x2,y2,x3,y3){
+function x_reflect(x1, y1,x2,y2){
   strokeWeight(2);
   stroke('black');
   s = [
@@ -16,53 +16,30 @@ function x_reflect(x1, y1,x2,y2,x3,y3){
     [0 ,-1, 0],
     [0 , 0, 1],
   ];
-  beginShape(line);
-  vertex(x1,y1);
-  vertex(x2,y2);
-  vertex(x3,y3);
-  vertex(x1,y1);
-  endShape();
   line(x1,y1,x2,y2);
   a = multiply(x1,y1,s);
   b = multiply(x2,y2,s);
-  c = multiply(x3,y3,s);
   stroke('red');
-  beginShape(line);
-  vertex(a[0],a[1]);
-  vertex(b[0],b[1]);
-  vertex(c[0],c[1]);
-  vertex(a[0],a[1]);
-  endShape();
+  translate(0,399);
+  line(a[0],a[1],b[0],b[1]);
   strokeWeight(0);
-  text("x-axis shear", 350 ,350);
-  strokeWeight(6);
-  point(330,345);
 }
 
-function y_shear(x1, y1,x2,y2,x3,y3, factor){
-  shy = factor;
+function y_reflect(x1, y1,x2,y2){
   strokeWeight(2);
   stroke('black');
   s = [
-    [1, 0, 0],
-    [shy ,1, 0],
+    [-1, 0, 0],
+    [0 , 1, 0],
     [0 , 0, 1],
   ];
   line(x1,y1,x2,y2);
   a = multiply(x1,y1,s);
   b = multiply(x2,y2,s);
-  c = multiply(x3,y3,s);
-  stroke('blue');
-  beginShape(line);
-  vertex(a[0],a[1]);
-  vertex(b[0],b[1]);
-  vertex(c[0],c[1]);
-  vertex(a[0],a[1]);
-  endShape();
+  stroke('red');
+  translate(370,0);
+  line(a[0],a[1],b[0],b[1]);
   strokeWeight(0);
-  text("y-axis shear", 350 ,370);
-  strokeWeight(6);
-  point(330,370);
 }
 
 function multiply(x,y,s){
